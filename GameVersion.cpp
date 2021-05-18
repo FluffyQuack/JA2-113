a53fb01b69ec12296b6022613e978ba713b90c32
FluffyQuack <git@fluffyquack.com>
2021-05-18 17:17:25 +0200

    Add project files.

---
+++ GameVersion.cpp
@@ -0,0 +1,62 @@
+#ifdef PRECOMPILEDHEADERS
+	#include "JA2 All.h"
+#else
+	#include "Types.h"
+	#include "GameVersion.h"
+#endif
+ 
+//
+//	Keeps track of the game version
+//
+ 
+// ------------------------------
+// MAP EDITOR (Release and Debug) BUILD VERSION
+// ------------------------------
+#ifdef JA2EDITOR
+	
+	#ifdef JA2UB
+		CHAR16 zVersionLabel[256]		= { L"Unfinished Business - Map Editor v1.13 (Development Build)" }; 
+	#else
+		CHAR16 zVersionLabel[256]		= { L"Map Editor v1.13 (Development Build)" }; 
+	#endif
+
+// ------------------------------
+// DEBUG BUILD VERSIONS
+// ------------------------------
+#elif defined JA2BETAVERSION
+
+	//DEBUG BUILD VERSION	
+	#ifdef JA2UB
+		CHAR16 zVersionLabel[256]		= { L"Debug: Unfinished Business - v1.13 (Development Build)" }; 
+	#elif defined (JA113DEMO)
+		CHAR16 zVersionLabel[256]		= { L"Debug: JA2 Demo - v1.13 (Development Build)" }; 
+	#else
+		CHAR16 zVersionLabel[256]		= { L"Debug: v1.13 (Development Build)" }; 
+	#endif
+
+#elif defined CRIPPLED_VERSION
+
+	//RELEASE BUILD VERSION s
+	CHAR16 zVersionLabel[256]		= { L"Beta v. 0.98" }; 
+
+// ------------------------------
+// RELEASE BUILD VERSIONS
+// ------------------------------
+#else
+
+	//RELEASE BUILD VERSION	
+	#ifdef JA2UB
+		CHAR16		zVersionLabel[256]		= { L"Release Unfinished Business - v1.13 (Development Build)" };
+	#elif defined (JA113DEMO)
+		CHAR16		zVersionLabel[256]		= { L"Release JA2 Demo - v1.13 (Development Build)" };
+	#else
+		CHAR16		zVersionLabel[256]		= { L"Release v1.13 (Development Build)" };
+	#endif
+
+#endif
+										
+CHAR8		czVersionNumber[16]	= { "Build 21.04.26" };		//YY.MM.DD
+CHAR16		zTrackingNumber[16]	= { L"Z" };
+CHAR16		zRevisionNumber[16] = { L"Revision 8980" };
+	
+// SAVE_GAME_VERSION is defined in header, change it there
---
+++ Strategic/Reinforcement.h
@@ -0,0 +1,16 @@
+#ifndef __REINFORCEMENT_H__
+#define __REINFORCEMENT_H__
+
+//For Autoresolve (mostly)
+void GetNumberOfEnemiesInFiveSectors( INT16 sSectorX, INT16 sSectorY, UINT8 *pubNumAdmins, UINT8 *pubNumTroops, UINT8 *pubNumElites, UINT8 *pubNumRobots, UINT8 *pubNumTanks, UINT8 *pubNumJeeps );
+BOOLEAN IsGroupInARightSectorToReinforce( GROUP *pGroup, INT16 sSectorX, INT16 sSectorY );
+UINT8 GetAdjacentSectors( UINT8 pSectors[4], INT16 sSectorX, INT16 sSectorY );
+UINT8 CountAllMilitiaInFiveSectors(INT16 sMapX, INT16 sMapY);
+UINT8 NumEnemiesInFiveSectors( INT16 sMapX, INT16 sMapY );
+
+//For Tactical
+UINT8 DoReinforcementAsPendingNonPlayer( INT16 sMapX, INT16 sMapY, UINT8 usTeam );
+void AddPossiblePendingMilitiaToBattle();
+GROUP* GetNonPlayerGroupInSector( INT16 sMapX, INT16 sMapY, UINT8 usTeam );
+
+#endif
